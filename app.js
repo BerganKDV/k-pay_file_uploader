b992840cd3de370b8816087462ab7d3f2132cb0d
jonlamb <jonnylamb@gmail.com>
2020-03-24 14:07:12 -0500

    More logs

--- app.js
+++ app.js
@@ -173,13 +173,16 @@ app.post('/upload', upload.fields(fields), function (req, res) {
         try {
             if (mappingFiles && mappingFiles.length !== 0) {
                 const csvTextFile = await fs.readFileAsync(mappingFiles[0].path, 'utf8');
-                console.log('CSV Text File', csvTextFile);
+                // console.log('CSV Text File', csvTextFile);
                 mappingObj = csvToObj(csvTextFile);
                 console.log('Mapping', mappingObj);
 
                 // Cleanup mapping file
                 fs.unlink(mappingFiles[0].path, (err) => {
-                    if (err) throw err;
+                    if (err) {
+                      console.log('There was a problem deleting the file', err);
+                      throw err;
+                    }
                 });
             }
 
